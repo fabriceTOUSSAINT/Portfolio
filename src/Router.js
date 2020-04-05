5db8b155f68001d890daf90d41937dceca7cbac6
Fabrice Toussaint <fabricetoussaint@Fabrices-MacBook-Pro.local>
2020-04-05 11:09:51 -0400

    portfolio spring cleaning, add ga tracker

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import FlexNav from './modules/FlexNav';
-import About from './pages/About';
-import Work from './pages/Work';
-import Photography from './pages/Photography';
+import FlexNav from "./FlexNav";
 
 export default (
-    <Router>
-        <div className="main">
-            <FlexNav />
-            <Route path='/About' component={About} />
-            <Route path='/Work' component={Work} />
-            <Route path='/Photography' component={Photography} />
-        </div>
-    </Router>
+  <Router>
+    <div className="main">
+      <FlexNav />
+    </div>
+  </Router>
 );
